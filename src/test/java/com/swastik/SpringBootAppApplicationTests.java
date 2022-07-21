fab88a8aa728bd23e8aa685276a338a323cbf4cf
shetty77 <swastikbantwal@gmail.com>
2022-07-21 10:36:21 +0530

    FIRST TIME COMMIT

---
+++ src/test/java/com/swastik/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swastik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
